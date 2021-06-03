8b4d7eb1443233c4a9cf3624d2cf29244b8a85bd
root <fazleh2010@gmail.com>
2021-06-03 20:45:34 +0200

    refactor

---
+++ index3.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    
+   <head>
+    
+    <title>Hackthon Question Answering (QueGG-Hackthon)</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>QueGG</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.3.0/mdb.min.css" integrity="sha512-aQX0wElRuvqZ1bVvsknO9jDXwvVI71vwYLuEDZ8QqMJxPozZPPCCmylpHx4ngmeIz7Bg6FfE4Ay+CfDVFqjy+A==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="/quegg/style.css"/>
+
+
+
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+    <div class="navbar-logo col-sm-2">
+        <img src="https://scdemo.techfak.uni-bielefeld.de/qahackathon/images/pal-logo.png" class="navbar-logo" />
+    </div>
+    <a class="navbar-brand col-sm" id="sidebar-brandlink" href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/index.php">Hackthon Question Answering (QueGG-Hackthon)</a>
+    <!--input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"-->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</nav>
+
+<div id="kopfbereich_gesamt">
+<div id="kopfbereich_folgeseite">
+<a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/index.php">home</a>&nbsp;&nbsp;|&nbsp;
+<a href="https://forms.gle/rTYj75FFUekWcWfM9">registration</a>&nbsp;&nbsp;|&nbsp;
+<a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/tutorial/coverage.php" target=top>Recipe</a></div></div>
+
+
+</head>
+
+<body onload="document.getElementById('statusdisplay').innerHTML = ''" id="top">
+<!--div id="kopfbereich_gesamt">
+<div id="kopfbereich_folgeseite">
+<a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/index.php">home</a>&nbsp;&nbsp;|&nbsp;
+<a href="https://forms.gle/rTYj75FFUekWcWfM9">registration</a>&nbsp;&nbsp;|&nbsp;
+<a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/tutorial/coverage.php" target=top>Recipe</a></div></div-->
+	
+
+        <div align="justify">
+	<!--h3>Hackthon on multilingual Question Answering over linked data (QueGG)</h3-->
+	
+	<p><strong>Hackashop description</strong></p>
+        <p>Many question answering systems over RDF that are induced from pairs of questions and answers using some machine learning technique suffer from a lack of controllability, making the governance and incremental improvement of the system challenging, not to mention the initial effort of collecting and providing training data. As an alternative, we present a model-based approach to question answering that uses an ontology lexicon in lemon format and automatically generates a lexicalized grammar that can be used to interpret and parse questions into SPARQL queries. The approach gives maximum control over the QA system to the developer of the system as every lexicon entry added to the lexicon increases the coverage of the grammar, and thus of the QA system, in a predictable way. </p>
+	
+        <p>The hackathon aims to improve the QA system on the combination of semantic web and language technologies. There are several possibilities to extend the system such as <p>
+        <ul>
+	    <li><a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/tutorial/coverage.php">Extend grammar coverage</a></li>
+	    <li><a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/tutorial/coverage.php">Extend the system for a new dataset</a></li>
+	    <li><a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/tutorial/coverage.php">Extend QA system for a language </a></li>
+	    <li><a href="https://scdemo.techfak.uni-bielefeld.de/qahackathon/tutorial/coverage.php">improve user interface</a>.</li>
+	    <li>We also will be very glad to know the creative ideas and directions from the participants</li>
+	</ul>
+
+        <p>We also will be very glad to know the creative ideas and directions from the participants.We welcome cross-disciplinary collaborations of computer scientists, computational linguistics and related fields to participate and contribute any level of their interest. To participate please register from <a href="https://forms.gle/rTYj75FFUekWcWfM9">here</a></p>
+
+        <p><strong>important links</strong></p>
+
+        <ul>
+	    <li>online demo <a href="https://scdemo.techfak.uni-bielefeld.de/quegg/" data-type="URL" data-id="https://scdemo.techfak.uni-bielefeld.de/quegg/">QA system user interface</a></li>
+	    <li>source code <a href="https://github.com/fazleh2010/question-grammar-generator/">grammar generator</a></li>
+	    <li>method <a href="http://ceur-ws.org/Vol-2722/nliwod2020-paper-2.pdf" data-type="URL" data-id="http://ceur-ws.org/Vol-2722/nliwod2020-paper-2.pdf">Generating Grammars from lemon lexica for QA system</a></li>
+	</ul>
+
+   
+
+	<p><strong>Organizing team</strong></p>
+        <p>Philipp Cimiano (CITEC - University Bielefeld, Germany), Chair<br>Mohammad Fazleh Elahi (CITEC - University Bielefeld, Germany)<br>Frank Grimm (CITEC - University Bielefeld, Germany)<br>Basil Ell (CITEC - University Bielefeld, Germany and Department of Informatics, University of Oslo, Norway)<br></p>
+	
+	<p><strong>References:</strong></p>
+	<p>Benz, V., Cimiano, P., Elahi, M.F., Ell, B. (2020), “Generating Grammars from lemon lexica for Questions Answering over Linked Data: a Preliminary Analysis.”. In: NLIWOD workshop at ISWC. vol. 2722, pp. 40–55. CEUR-WS.org (2020).<a href="http://ceur-ws.org/Vol-2722/nliwod2020-paper-2.pdf" data-type="URL" data-id="http://ceur-ws.org/Vol-2722/nliwod2020-paper-2.pdf">[pdf]</a> </p>
+	
+</div></div>
+</body> 
+
+
+
+</html>
+

4b27cdae0c96fb7cd9be002db27b1e2a3c2cdeab
root <fazleh2010@gmail.com>
2021-06-08 11:10:41 +0200

    refactor

--- tutorial/coverage.php
+++ tutorial/coverage.php
@@ -323,13 +323,12 @@ Example2:
 <div class="section" id="recipe-new-language">
 <h3><a class="toc-backref" href="#id104">2.4 Recipe</a><a class="headerlink" href="#base-file" title="Permalink to this headline"></a></h3>
 <pre>
-<ul>
-  <li>Create a branch of the <a href="https://github.com/fazleh2010/question-grammar-generator.git">QueGG project</a>. Alternatively, fork the project. </li>
-  <li>Write a base lemon.</li>
-  <li>Write sentence templates.</li>
-  <li>Write lexical entries as discussed in <a href="http://localhost/tutorial/coverage.php#id3">Section 1</a>.</li>
-  <li>Build and run the program following the <a href="https://github.com/fazleh2010/question-grammar-generator.git">instructions</a>.</li>
-</ul>
+
+   (i) Create a branch of the <a href="https://github.com/fazleh2010/question-grammar-generator.git">QueGG project</a>. Alternatively, fork the project.
+   (ii) Write a base lemon.
+   (iii) Write sentence templates.
+   (iv) Write lexical entries as discussed in <a href="http://localhost/tutorial/coverage.php#id3">Section 1</a>.
+   (v) Build and run the program following the <a href="https://github.com/fazleh2010/question-grammar-generator.git">instructions</a>
 </pre>
 <p></p><div>
 
